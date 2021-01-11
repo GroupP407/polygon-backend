b4bed247d7684d9c27dc6005b22119753f08789c
nicshek <nicshekatw@gmail.com>
2021-01-11 13:47:43 +0300

    Добавил jaeger в зависимости чарта инфраструктуры, добавил elasticsearch для хранения данных jaeger. Подключил отправку трэйсов в jaeger, библиотека используемая для этого находится в стадии бэты.

--- src/Polygon.API/Startup.cs
+++ src/Polygon.API/Startup.cs
@@ -1,16 +1,17 @@
 using System.Reflection;
-using App.Metrics;
-using App.Metrics.Extensions.Configuration;
-using App.Metrics.Formatters.Ascii;
 using App.Metrics.Formatters.Prometheus;
 using Autofac;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using OpenTelemetry;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 namespace Polygon.API
 {
@@ -25,6 +26,15 @@ namespace Polygon.API
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOpenTelemetryTracing(builder =>
+            {
+                builder
+                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("polygon"))
+                    .AddAspNetCoreInstrumentation()
+                    .AddSqlClientInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .AddJaegerExporter(options => Configuration.Bind("JaegerExporterOptions", options));
+            });
             services.AddAppMetricsCollectors();
             services.AddControllers();
             services.AddAutoMapper(Assembly.Load("Polygon.API"));
@@ -58,7 +68,7 @@ namespace Polygon.API
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints => endpoints.MapControllers());
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
 
         public void ConfigureContainer(ContainerBuilder builder)
